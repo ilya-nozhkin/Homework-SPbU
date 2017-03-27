d04fb22a8e26560e59063372cb8d1cebe68d6fa1
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-03-27 23:33:59 +0300

    implemented GUI for calculator

---
+++ 1st_course/2nd_semester/5th_homework/task_1/src/s2_144/nozhkin/task5_1/Main.java
@@ -0,0 +1,22 @@
+package s2_144.nozhkin.task5_1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+        primaryStage.setTitle("Simple calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ 1st_course/2nd_semester/5th_homework/task_1/test/CalculatorTest.java
@@ -0,0 +1,78 @@
+import javafx.application.Application;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
+import javafx.stage.Stage;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import s2_144.nozhkin.task5_1.Main;
+
+
+public class CalculatorTest extends Application {
+    private static Stage primaryStage;
+    private static AssertionError assertion = null;
+
+    @Test
+    public void test() {
+        assertion = null;
+        CalculatorTest.launch(CalculatorTest.class);
+        if (assertion != null)
+            throw assertion;
+    }
+
+    public static <T> T findNode(String selector) {
+        return (T) primaryStage.getScene().lookup(selector);
+    }
+
+    public void calculationTest() {
+        Spinner<Double> leftOperand = findNode("#leftOperand");
+        Spinner<Double> rightOperand = findNode("#rightOperand");
+        ComboBox<String> operation = findNode("#operation");
+        Label result = findNode("#result");
+
+        leftOperand.getValueFactory().setValue(10.0);
+        rightOperand.getValueFactory().setValue(2.0);
+
+        operation.getSelectionModel().select("+");
+        Assert.assertTrue(result.getText().equals("12.0"));
+
+        operation.getSelectionModel().select("-");
+        Assert.assertTrue(result.getText().equals("8.0"));
+
+        operation.getSelectionModel().select("*");
+        Assert.assertTrue(result.getText().equals("20.0"));
+
+        operation.getSelectionModel().select("/");
+        Assert.assertTrue(result.getText().equals("5.0"));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
+        Parent root = FXMLLoader.load(Main.class.getResource("Calculator.fxml"));
+        primaryStage.setTitle("Simple calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        try {
+            calculationTest();
+        } catch (AssertionError e) {
+            assertion = e;
+            primaryStage.close();
+        }
+
+        primaryStage.close();
+    }
+}
