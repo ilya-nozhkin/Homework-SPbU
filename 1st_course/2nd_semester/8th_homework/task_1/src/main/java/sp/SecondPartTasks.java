bcad8ab30a90bedcdeb6f7014da64889df36a4f1
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-05-09 20:41:48 +0300

    implemented the first part

---
+++ 1st_course/2nd_semester/8th_homework/task_1/src/main/java/sp/SecondPartTasks.java
@@ -0,0 +1,33 @@
+package sp;
+
+import java.util.List;
+import java.util.Map;
+
+public final class SecondPartTasks {
+
+    private SecondPartTasks() {}
+
+    // Найти строки из переданных файлов, в которых встречается указанная подстрока.
+    public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
+        throw new UnsupportedOperationException();
+    }
+
+    // В квадрат с длиной стороны 1 вписана мишень.
+    // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
+    // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
+    public static double piDividedBy4() {
+        throw new UnsupportedOperationException();
+    }
+
+    // Дано отображение из имени автора в список с содержанием его произведений.
+    // Надо вычислить, чья общая длина произведений наибольшая.
+    public static String findPrinter(Map<String, List<String>> compositions) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
+    // Необходимо вычислить, какой товар и в каком количестве надо поставить.
+    public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ 1st_course/2nd_semester/8th_homework/task_1/src/test/java/sp/SecondPartTasksTest.java
@@ -0,0 +1,28 @@
+package sp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SecondPartTasksTest {
+
+    @Test
+    public void testFindQuotes() {
+        fail();
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+        fail();
+    }
+
+    @Test
+    public void testFindPrinter() {
+        fail();
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+        fail();
+    }
+}
