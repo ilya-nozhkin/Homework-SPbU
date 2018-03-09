44918d2523a9ef7c7511687fb499df0aaf2fe438
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-03-09 22:18:58 +0300

    replaced 'split' with 'words'

--- 2nd_course/2nd_semester/2nd_homework/task_4/main.hs
+++ 2nd_course/2nd_semester/2nd_homework/task_4/main.hs
@@ -3,14 +3,6 @@ find x = head' . filter ((== x) . snd) . zip [1, 2..] where
     head' [] = Nothing
     head' (e:es) = Just (fst e)
 
-split :: String -> [String]
-split s = split' s "" [] where
-    split' "" "" r = reverse r
-    split' "" t r = reverse ((reverse t):r)
-    split' (' ':cs) "" r = split' cs "" r
-    split' (' ':cs) t r = split' cs "" ((reverse t):r)
-    split' (c:cs) t r = split' cs (c:t) r 
-
 main :: IO()
 main = do
     putStr "Enter the list of numbers divided by space: " 
@@ -18,7 +10,7 @@ main = do
     putStr "Enter the number contained in this list: "
     numberLine <- getLine
 
-    let list = map (\x -> read x :: Integer) (split listLine)
+    let list = map (\x -> read x :: Integer) (words listLine)
     let number = read numberLine
     let output = find number list 
 
