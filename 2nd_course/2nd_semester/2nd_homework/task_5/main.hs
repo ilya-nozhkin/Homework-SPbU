cac7d03330309e750eba3619ae207fad5ffd5e50
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-03-09 22:25:55 +0300

    replaced 'split' with 'words'

--- 2nd_course/2nd_semester/2nd_homework/task_5/main.hs
+++ 2nd_course/2nd_semester/2nd_homework/task_5/main.hs
@@ -6,14 +6,6 @@ sumLists a b c = ((head' a) + (head' b) + (head' c)):(sumLists (tail' a) (tail' 
     tail' [] = []
     tail' (e:es) = es
 
-split :: String -> [String]
-split s = split' s "" [] where
-    split' "" "" r = reverse r
-    split' "" t r = reverse ((reverse t):r)
-    split' (' ':cs) "" r = split' cs "" r
-    split' (' ':cs) t r = split' cs "" ((reverse t):r)
-    split' (c:cs) t r = split' cs (c:t) r 
-
 main :: IO()
 main = do
     putStr "Enter the first list of numbers divided by space: " 
@@ -23,9 +15,9 @@ main = do
     putStr "Enter the third list of numbers divided by space: " 
     listLine3 <- getLine
 
-    let list1 = map (\x -> read x :: Integer) (split listLine1)
-    let list2 = map (\x -> read x :: Integer) (split listLine2)
-    let list3 = map (\x -> read x :: Integer) (split listLine3)
+    let list1 = map (\x -> read x :: Integer) (words listLine1)
+    let list2 = map (\x -> read x :: Integer) (words listLine2)
+    let list3 = map (\x -> read x :: Integer) (words listLine3)
     let output = sumLists list1 list2 list3
 
     putStrLn ("List of sum of elements of those lists: " ++ show output)
