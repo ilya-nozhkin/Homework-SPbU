581d7a0b4c32626950c7e985b1ffc71e638084d6
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-03-20 11:01:46 +0300

    added empty actions and case when n = 0

--- 2nd_course/2nd_semester/test/task_3/main.hs
+++ 2nd_course/2nd_semester/test/task_3/main.hs
@@ -9,8 +9,9 @@ getRow a n = getRow' (l * 2 - 1) (a - l) "" where
     getRow' x s r = getRow' (x - 1) s ('x':r)
 
 printDiamond :: Integer -> IO()
+printDiamond 0 = return ()
 printDiamond a = printDiamond' a (a * 2 - 1) where
-    printDiamond' a 1 = putStrLn (id (getRow a 1))
-    printDiamond' a l = do
-        putStrLn (id (getRow a l))
+    printDiamond' a 0 = return ()
+    printDiamond' a l = 
+        putStrLn (id (getRow a l)) >>
         printDiamond' a (l - 1)
