75f748267aad7393bc5cf1359e1c605414350c43
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-03-20 09:54:55 +0300

    implemented matrix generation

---
+++ 2nd_course/2nd_semester/test/task_2/main.hs
@@ -0,0 +1,6 @@
+ones :: Int -> Int -> [Int]
+ones l n = take n ((take l [1,1..]) ++ [0,0..])
+
+matrix n = matrix' n (n - 1) [take n [n,n..]] where
+    matrix' n 0 m = m
+    matrix' n l (r:m) = matrix' n (l - 1) ((zipWith (-) r (ones l n)):r:m)
