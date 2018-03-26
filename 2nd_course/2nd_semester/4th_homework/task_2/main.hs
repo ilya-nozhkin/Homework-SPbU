201478b86437a48a0af412f7ac56610b4a17d268
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-03-26 21:46:10 +0300

    implemented user interface for sorted list of numbers

--- 2nd_course/2nd_semester/4th_homework/task_2/main.hs
+++ 2nd_course/2nd_semester/4th_homework/task_2/main.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 insertIntoSorted :: Integer -> [Integer] -> [Integer]
 insertIntoSorted e l = insertIntoSorted' e l where
     insertIntoSorted' e [] = [e]
@@ -9,8 +11,12 @@ getCommand :: IO [Integer]
 getCommand = getLine >>= 
     return . (map (\w -> read w :: Integer)) . words
 
-interpret :: [Integer] -> [Integer] -> [Integer]
-interpret l [0] = l 
+interpret :: [Integer] -> [Integer] -> IO ([Integer])
+interpret l [0] = return l 
 interpret l (1:v:[]) = getCommand >>= (interpret (insertIntoSorted v l))
+interpret l (2:v:[]) = getCommand >>= (interpret (delete v l))
+interpret l [3] = (putStrLn . show) l >> getCommand >>= interpret l
+interpret l _ = putStrLn "Unknown operation" >> getCommand >>= interpret l
 
-main = getCommand >>= return . (interpret [])
+main :: IO ()
+main = getCommand >>= interpret [] >> return ()
