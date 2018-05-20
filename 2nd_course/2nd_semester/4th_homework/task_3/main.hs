27ecd7ed16be91763a11f5128adc71e67d10840e
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-05-20 21:30:07 +0300

    changed stop condition

--- 2nd_course/2nd_semester/4th_homework/task_3/main.hs
+++ 2nd_course/2nd_semester/4th_homework/task_3/main.hs
@@ -3,7 +3,6 @@ import Data.Tree
 --BFS
 elems :: Tree a -> [a]
 elems t = (concatMap (\(_, r) -> r) . 
-           (\(a, b) -> a ++ [head b]) .
-           span (\(c, _) -> not (null c)) . 
+           takeWhile (\(c, r) -> (not (null c)) || (not (null r))) . 
            iterate (\(c, r) -> (concatMap (subForest) c, (map (rootLabel) c))))
            ([t], [])
