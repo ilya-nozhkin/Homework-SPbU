96dd4b4240f4ff8cb16557f99a367a0b157421a1
Ilya Nozhkin <nozhkin.ii@gmail.com>
2018-05-22 22:53:23 +0300

    implemented finding an element of list which is greater than its neighbours using monadic functions

---
+++ 2nd_course/2nd_semester/5th_homework/task_3/main.hs
@@ -0,0 +1,10 @@
+import Control.Monad.Zip
+import Data.List
+
+mzip3 a b c = mzipWith (\x (y, z) ->  (x, y, z)) a (mzip b c)
+
+findHill :: Ord b => [b] -> Maybe b
+findHill (a:b:c:xs) = (\l -> if (null l) then Nothing else Just (head l))
+    ((mzip3 (a:b:c:xs) (b:c:xs) (c:xs)) >>= 
+        (\(x, y, z) -> if (y > x) && (y > z) then [y] else []))
+findHill _ = Nothing
